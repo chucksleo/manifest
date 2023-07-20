ce37017edd9ba347edb3b18d54c730e8e7326633
Leo <you@example.com>
2023-07-20 05:32:41 +0100

    Added Annotations and labels

--- main.tf
+++ main.tf
@@ -54,3 +54,30 @@ resource "kubernetes_service" "my_service" {
     type = "ClusterIP"
   }
 }
+
+resource "kubernetes_annotations" "example" {
+  api_version = "apps/v1"
+  kind        = "Deployment"
+  metadata {
+    name = "my-config"
+  }
+  # These annotations will be applied to the Deployment resource itself
+  annotations = {
+    "owner" = "myteam"
+  }
+  # These annotations will be applied to the Pods created by the Deployment
+  template_annotations = {
+    "owner" = "myteam"
+  }
+}
+
+resource "kubernetes_labels" "example" {
+  api_version = "v1"
+  kind        = "ConfigMap"
+  metadata {
+    name = "my-config"
+  }
+  labels = {
+    "owner" = "myteam"
+  }
+}
